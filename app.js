00c3f1858f682cf64e59446b4cba9542760a229c
rabib-jahin <1805003@ugrad.cse.buet.ac.bd>
2020-06-28 23:18:37 +0600

    ff

---
+++ app.js
@@ -0,0 +1,28 @@
+var express=require('express');
+var socket=require('socket.io')
+var app=express();
+
+
+var server=app.listen(5000,()=>{
+
+	console.log("listening");
+
+});
+app.use(express.static('public'))
+
+var io=socket(server)
+
+io.on('connection',function(socket){
+
+	console.log('made socket connection',socket.id )
+socket.on('chat',function(data){
+
+io.sockets.emit('chat',data)
+
+
+})
+socket.on('typing',(data)=>{
+socket.broadcast.emit('typing',data)
+
+})
+})
---
+++ public/chat.js
@@ -0,0 +1,35 @@
+var socket=io.connect()
+
+var message=document.getElementById('message');
+handle=document.getElementById('handle');
+btn=document.getElementById('send');
+output=document.getElementById('output');
+feedback=document.getElementById('feedback');
+btn.addEventListener('click',()=>{
+socket.emit('chat',{
+
+	message:message.value,
+	handle:handle.value
+})
+message.value=""
+})
+
+message.addEventListener('keypress',()=>{
+
+	socket.emit('typing',handle.value)
+})
+message.addEventListener('ontouchstart',()=>{
+
+	socket.emit('typing',handle.value)
+})
+
+socket.on('chat',function(data){
+feedback.innerHTML=""
+output.innerHTML+="<p><strong>"+data.handle+":</strong> "+data.message+"</p"
+
+})
+socket.on('typing',(data)=>{
+
+feedback.innerHTML="<p><em>"+data+" is typing...."+"</p></em>"
+
+})
