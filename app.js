96cdc3a1d25a535018d8e8f03d05ef594dad5b00
rabib-jahin <1805003@ugrad.cse.buet.ac.bd>
2020-06-29 14:31:42 +0600

    updata

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express=require('express');
 var socket=require('socket.io')
 var app=express();
-
+let client=0
+let online=[]
 var port=process.env.PORT||5000;
 var server=app.listen(port,()=>{
 
@@ -13,14 +14,37 @@ app.use(express.static('public'))
 var io=socket(server)
 
 io.on('connection',function(socket){
-
+client++;
 	console.log('made socket connection',socket.id )
 socket.on('chat',function(data){
+
 
 io.sockets.emit('chat',data)
 
 
 })
+
+socket.on('disconnect',()=>{
+client--;
+
+})
+socket.on('handle',(data)=>{
+	if(data.handle!==''){
+online.push(data.handle)
+console.log(data.handle)}
+
+
+})
+socket.on('get',()=>{
+
+	io.sockets.emit('get',{
+
+clients:client,
+on:online
+
+	})
+})
+
 socket.on('typing',(data)=>{
 socket.broadcast.emit('typing',data)
 
--- public/chat.js
+++ public/chat.js
@@ -5,12 +5,41 @@ handle=document.getElementById('handle');
 btn=document.getElementById('send');
 output=document.getElementById('output');
 feedback=document.getElementById('feedback');
+show=document.getElementById('show');
+part=document.getElementById('participents');
+list=document.getElementById('list');
+sethandle=document.getElementById('sethandle');
+var div=document.getElementById('chat-window');
+let count=0;
+let handles=""
+
+
+
+sethandle.addEventListener('click',()=>{
+socket.emit('handle',{
+	handle:handle.value
+})
+
+})
+part.addEventListener('click',()=>{
+
+socket.emit('get')
+
+})
 btn.addEventListener('click',()=>{
+	
+
+	
+
 socket.emit('chat',{
 
 	message:message.value,
-	handle:handle.value
+    handle:handle.value
 })
+
+
+
+
 message.value=""
 })
 
@@ -18,7 +47,7 @@ message.addEventListener('keypress',()=>{
 
 	socket.emit('typing',handle.value)
 })
-message.addEventListener('ontouchstart',()=>{
+message.addEventListener('touchstart',()=>{
 
 	socket.emit('typing',handle.value)
 })
@@ -26,10 +55,20 @@ message.addEventListener('ontouchstart',()=>{
 socket.on('chat',function(data){
 feedback.innerHTML=""
 output.innerHTML+="<p><strong>"+data.handle+":</strong> "+data.message+"</p"
-
+div.scrollTop=div.scrollHeight
 })
 socket.on('typing',(data)=>{
 
 feedback.innerHTML="<p><em>"+data+" is typing...."+"</p></em>"
+div.scrollTop=div.scrollHeight
 
+})
+socket.on('get',(data)=>{
+let online=data.on.map((data)=>{
+return "<li>"+ data+"</li>"
+
+}).join('')
+
+	show.innerHTML="<p> "+ data.clients+" joined </p>"
+	list.innerHTML=online
 })
