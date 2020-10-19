befb071b2d02aaafd80220f7121d36afea0b5eb6
rabib-jahin <1805003@ugrad.cse.buet.ac.bd>
2020-10-19 12:46:00 +0600

    commit

--- app.js
+++ app.js
@@ -1,56 +1,134 @@
 var express=require('express');
-var socket=require('socket.io')
+var socket=require('socket.io');
+var fd=require("fs");
 var app=express();
 let client=0
 let online=[]
 var port=process.env.PORT||5000;
+var rooms=[]
+var obj={}
+var obj2={}
+var id={}
+
 var server=app.listen(port,()=>{
 
 	console.log("listening");
 
 });
-app.use(express.static('public'))
+
+
+
+
+
+
+app.use(express.static(__dirname+'/public'));
+
+
+
 
 var io=socket(server)
 
 io.on('connection',function(socket){
+
+
+
+
 client++;
 	console.log('made socket connection',socket.id )
+
+
+
+
 socket.on('chat',function(data){
 
+room=data.room;
+io.sockets.to(data.room).emit('chat',data)
 
-io.sockets.emit('chat',data)
+
+})
+socket.on('room',data=>{
+
+    if(data.room!=""&&  obj2&& !(data.room in obj2)  ){
+	rooms.push(data.room);
+	obj2[data.room]=data.password
+	socket.join(data.room);
+
+
+socket.emit('room',{
+	id:1,
+	room:data.room
+});
+
+
+}
+else{
+socket.emit('room',{
+	id:0
+});	
+}
+
+
+
+
+
 
 
 })
 
-socket.on('disconnect',()=>{
+socket.on('dis',(data)=>{
+
 client--;
+if(id[socket.id]!=null);
+socket.to(data).broadcast.emit('dis',id[socket.id])
+
+socket.disconnect();
+
+//obj[data.room].splice(obj[data.room].indexOf(id[socket.id]),1);
+console.log(socket.id+" disconnected")
 
 })
-socket.on('handle',(data)=>{
-	if(data.handle!==''){
-online.push(data.handle)
-console.log(data.handle)}
 
+socket.on('handle',(data)=>{
+online.push(data.handle)
+id[socket.id]=data.handle;
+console.log(data.handle)
+socket.to(data.room).broadcast.emit('handle',data);
 
 })
 socket.on('image',(data)=>{
-io.sockets.emit('image',data)
+io.sockets.to(data.room).emit('image',data)
+
+ })
+socket.on('getRooms',(data)=>{
+	if(rooms.includes(data.room) && obj2[data.room]===data.password){
+		socket.join(data.room);
+		socket.emit('getRooms',{
+
+			room:data.room,
+			id:1
+		})
+	}
+	else
+		socket.emit('getRooms',{
+			id:0
+		});
 
 })
-socket.on('get',()=>{
+/*socket.on('get',(data)=>{
 
-	io.sockets.emit('get',{
+	socket.emit('get',{
 
 clients:client,
-on:online
+on:online,
+room:data
 
 	})
 })
-
+*/
 socket.on('typing',(data)=>{
-socket.broadcast.emit('typing',data)
+socket.to(data.room).broadcast.emit('typing',data)
 
 })
+
+
 })
--- public/chat.js
+++ public/chat.js
@@ -3,49 +3,107 @@ var socket=io.connect()
 var message=document.getElementById('message');
 handle=document.getElementById('handle');
 btn=document.getElementById('send');
-output=document.getElementById('output');
+//output=document.getElementById('output');
 feedback=document.getElementById('feedback');
 show=document.getElementById('show');
-part=document.getElementById('participents');
-list=document.getElementById('list');
+//part=document.getElementById('participents');
+//list=document.getElementById('list');
 sethandle=document.getElementById('sethandle');
 file=document.getElementById('fileName');
 image=document.getElementById('image');
+handleDiv=document.getElementById('handlediv')
+room=document.getElementById('roomName')
+rooms=document.getElementById('rooms')
+joinRoom=document.getElementById('joinRoom');
+create=document.getElementById("save");
+//li=document.getElementById("uList");
+leave=document.getElementById("leave");
+title=document.getElementById("title")
 var div=document.getElementById('chat-window');
+
 
 let count=0;
 let handles=""
+let roomVal;
+joinRoom.addEventListener('click',()=>{
+roomVal=room.value;
 
+m=prompt("Enter the password");
+socket.emit('getRooms',{
 
+	room:room.value,
+	password:m
+});
 
+})
+create.addEventListener('click',()=>{
+let s;
+roomVal=room.value
+if(room.value){
+
+		s=prompt("Set a password ");
+
+	}
+	else{
+		alert("Room with this name can't be created");
+	}
+socket.emit('room',{
+
+	room:room.value,
+	password:s
+})
+
+})
 sethandle.addEventListener('click',()=>{
-socket.emit('handle',{
-	handle:handle.value
-})
 
+if(handle.value==''){
+	alert('Please enter your handle');
+}
+else{
+	socket.emit('handle',{
+	handle:handle.value,
+	room:roomVal
 })
-part.addEventListener('click',()=>{
-
-socket.emit('get')
-
+handleDiv.style.display='none';
+alert("hello "+handle.value+" Welcome to this chat app");
+}
 })
+/*part.addEventListener('click',()=>{
+
+socket.emit('get',room.value)
+
+})*/
 btn.addEventListener('click',()=>{
 	
 
 	
+if(handle.value==''){
+	alert('Please enter your handle');
 
+}
+else if(message.value==''){
+	alert("enter a text");
+}
+else{
 socket.emit('chat',{
 
 	message:message.value,
-    handle:handle.value
+    handle:handle.value,
+    room:roomVal
 })
-
+}
 
 
 
 message.value=""
+message.focus();
 })
+leave.addEventListener('click',()=>{
 
+socket.emit('dis',roomVal);
+
+location.reload();
+})
 
 file.addEventListener('change',(element)=>{
 console.log(element)
@@ -57,7 +115,8 @@ console.log("RESULT",reader.result);
 socket.emit('image',{
 
 	result:reader.result,
-	handle:handle.value
+	handle:handle.value,
+	room:roomVal
 
 
 }
@@ -69,38 +128,113 @@ reader.readAsDataURL(file);
 
 message.addEventListener('keypress',()=>{
 
-	socket.emit('typing',handle.value)
+	socket.emit('typing',{
+
+		handle:handle.value,
+		room:roomVal
+	})
 })
 message.addEventListener('touchstart',()=>{
 
-	socket.emit('typing',handle.value)
+	socket.emit('typing',{
+
+handle:handle.value,
+		room:roomVal
+
+	})
 })
 
 socket.on('chat',function(data){
+	var div2=document.createElement("div");
+div2.classList.add("output");
+
 feedback.innerHTML=""
-output.innerHTML+="<p><strong>"+data.handle+":</strong> "+data.message+"</p"
+div2.innerHTML="<p><strong>"+data.handle+":</strong> "+data.message+"</p"
+div.appendChild(div2);
 div.scrollTop=div.scrollHeight
 })
 socket.on('typing',(data)=>{
 
-feedback.innerHTML="<p><em>"+data+" is typing...."+"</p></em>"
+feedback.innerHTML="<p><em>"+data.handle+" is typing...."+"</p></em>"
 div.scrollTop=div.scrollHeight
 
 })
-socket.on('get',(data)=>{
+/*socket.on('get',(data)=>{
 let online=data.on.map((data)=>{
-return "<li>"+ data+"</li>"
+return "<li>"+ data+"</li>" 
 
 }).join('')
 
-	show.innerHTML="<p> "+ data.clients+" joined </p>"
+
 	list.innerHTML=online
-})
+})*/
 socket.on('image',(data)=>{
+	var div2=document.createElement("div");
+div2.classList.add("output");
+
 if(data.result.includes('/pdf'))
-output.innerHTML+="<p><strong>"+data.handle+":</strong>"+"<iframe class=\"pdf\" src=\""+data.result+"\"/>"
+div2.innerHTML="<p><strong>"+data.handle+":</strong>"+"<embed class=\"pdf\" src=\""+data.result+"\"/>"
 else
-output.innerHTML+="<p><strong>"+data.handle+":</strong>"+"<img class=\"image\" src=\""+data.result+"\"/>"
+div2.innerHTML="<p><strong>"+data.handle+":</strong>"+"<img class=\"image\" src=\""+data.result+"\"/>"
+
+div.appendChild(div2);
+div.scrollTop=div.scrollHeight
+})
+/*
+socket.on('getRooms',data=>{
+let online=data.map((datum)=>{
+return "<li>"+ datum+"</li>" 
+
+}).join('')
+
+li.innerHTML=online
+
+
+})*/
+socket.on('dis',data=>{
+var div2=document.createElement("div");
+div2.classList.add("output");
+//leave.style.display="none";
+
+feedback.innerHTML=""
+div2.innerHTML="<p><strong>"+"Bot"+":</strong> "+data+" has left this room"+"</p"
+div.appendChild(div2);
+div.scrollTop=div.scrollHeight
+
+})
+socket.on('room',data=>{
+if(data.id){
+	alert("succesfully created the room")
+title.innerHTML="Welcome to "+data.room +" Room"
+leave.style.display="block";
+
+}
+else{
+	alert("Can't create")
+}
+	
+})
+socket.on('getRooms',data=>{
+
+	if(data.id){
+		alert("successfully joined");
+		title.innerHTML="Welcome to "+data.room +" Room"
+		leave.style.display="block";
+	}
+	else{
+
+		alert("Can't join.Room doesn't exist")
+	}
+})
+
+socket.on('handle',data=>{  
+var div2=document.createElement("div");
+div2.classList.add("output");
+
+feedback.innerHTML=""
+div2.innerHTML="<p><strong>"+"Bot"+":</strong> "+data.handle+" has joined this room"+"</p"
+div.appendChild(div2);
+div.scrollTop=div.scrollHeight
 
 
 })
